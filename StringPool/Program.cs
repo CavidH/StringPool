e5f26cab8c077429e336866ca173aa07b76d7abe
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-04-10 18:24:17 +0400

    Initial commit

---
+++ StringPool/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Helllo";
+            string s1 = "Helllo";
+            string s2 = "Helllo";
+            string s3 = "Helllo";
+            string s4 = new string(new []{'d','d'});
+            Console.WriteLine(s.GetHashCode());
+            Console.WriteLine(s1.GetHashCode());
+            Console.WriteLine(s3.GetHashCode());
+            Console.WriteLine(s3.GetHashCode());
+            Console.WriteLine(s4.GetHashCode());
+        }
+    }
+}
